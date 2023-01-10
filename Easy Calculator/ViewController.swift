fe339adbf774c973934e5f8e3a503ed34a98b96a
Arif <arifdemirkoparan@icloud.com>
2023-01-10 17:51:55 +0300

    Initial Commit

---
+++ Easy Calculator/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  Easy Calculator
+//
+//  Created by Arif Demirkoparan on 10.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var textfield1: UITextField!
+    @IBOutlet weak var textfield2: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    var senderTitle:String?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func clickToCalculate(_ sender: UIButton) {
+        senderTitle = sender.titleLabel?.text!
+        if let IntOne = textfield1.text , let IntTwo = textfield2.text {
+            if let valueOne = Int(IntOne) , let valueTwo = Int(IntTwo) {
+                hesapla(sayi1: valueOne, sayi2: valueTwo)
+            }else {
+                resultLabel.text = "You entered a non-digit value."
+            }
+        }
+    }
+    
+    func hesapla(sayi1:Int,sayi2:Int) {
+        if senderTitle! == "Add" {
+            let result =  sayi1 + sayi2
+            resultLabel.text = "Result : \(result)"
+        }
+        if senderTitle! == "Subtract" {
+            let result =  sayi1 - sayi2
+            resultLabel.text = "Result : \(result)"
+        }
+        if senderTitle! == "Multiply" {
+            let result =  sayi1 * sayi2
+            resultLabel.text = "Result : \(result)"
+        }
+        if senderTitle! == "Divide" {
+            let result =  sayi1 / sayi2
+            resultLabel.text = "Result : \(result)"
+        }
+        
+    }
+    
+}
+
